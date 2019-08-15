fe8ef6f9139fa3b5f81bc6dd426fd1de2490a4e0
Aakanksha NS <aakanksha@unicorn.attlocal.net>
2019-08-14 23:52:06 -0700

    flower classifier initialize

---
+++ flask-server/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from fastai.basic_train import load_learner
+from fastai.vision import open_image
+app = Flask(__name__)
+
+# load the learner
+learn = load_learner(path='.', file='trained_model.pkl')
+classes = learn.data.classes
+
+
+def predict_single(img_file):
+    'function to take image and return prediction'
+    prediction = learn.predict(open_image(img_file))
+    probs_list = prediction[2].numpy()
+    return {
+        'category': classes[prediction[1].item()],
+        'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
+    }
+
+
+# route for prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    return jsonify(predict_single(request.files['image']))
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
